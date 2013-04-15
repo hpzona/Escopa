c243182359221d9a94e0141be4c864a505081383
murilotf <murilotf.m4@gmail.com>
2013-04-15 11:44:30 -0300

    Ajeitando super System

--- Escopa/src/Principal.java
+++ Escopa/src/Principal.java
@@ -11,7 +11,7 @@ public class Principal {
   
     //teste
     public static void main(String args[]){
- System.out.println("teste")   ;
-
+        
+         System.out.println("teste");
     }
 }
